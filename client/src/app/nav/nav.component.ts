ab7453b1c36a7474dfaaf601b4b9215554e650c3
Didemudia <didemudia@yahoo.co.uk>
2021-09-13 14:12:50 +0100

    Register completed

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent implements OnInit {
+  model: any = {}
+  // loggedIn: boolean = false;
+
+
+  constructor(public accountService: AccountService) {}
+
+  ngOnInit(): void {
+    // this.getCurrentUser();
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+
+
+}
